a6ce5453cc417c2e24847f2bd5af0baa7801e468
KUMMARISREEDHAR <sreedharsri2003@gmail.com>
2024-01-28 15:13:57 +0530

    enclass

---
+++ javabeginner/src/com/cs/encapsulation/EncapDemo1.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.cs.encapsulation;
+
+/**
+ * 
+ */
+public class EncapDemo1 {
+
+	
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public int getRoolnum() {
+		return roolnum;
+	}
+
+	public void setRoolnum(int roolnum) {
+		this.roolnum = roolnum;
+	}
+
+	private String sname;
+	
+	private int roolnum;
+	
+	public String currentName() {
+		return sname;
+		
+		
+	}
+}
---
+++ javabeginner/src/com/cs/encapsulation/EncapsualtDrive1.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.cs.encapsulation;
+
+/**
+ * 
+ */
+public class EncapsualtDrive1 {
+
+	public static void main(String[] args) {
+		
+		EncapDemo1 en1=new EncapDemo1();
+		en1.setSname("ss");
+		en1.currentName();
+	}
+}
